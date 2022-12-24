fb397c6089830c507e4f425dadab05b1098adca6
Prankur Sharma <prankur.sharma2018@vitbhopal.ac.in>
2022-12-24 20:07:13 +0530

    Project

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const axios = require("axios");
-var cacheService = require("express-api-cache");
+var cacheService = require("express-api-cache"); //library used for caching
 var cache = cacheService.cache;
 const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.use(express.json());
+
+//data cached in the server for 2 hours and then refreshed to get new data from api
 
 app.use(cache('120 minutes'));
 
